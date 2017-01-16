c17a77e5e4db91aaf074bacd1a04e43bf930413d
LEGOlord208 <LEGOlord208@gmail.com>
2017-01-16 11:13:01 +0100

    Abort!

---
+++ shell_linux.go
@@ -0,0 +1,10 @@
+package main;
+
+import (
+	"os/exec"
+	"strconv"
+)
+
+func makecmd(name string, arg string) *exec.Cmd{
+	return exec.Command("sh", "-c", "./" + strconv.Quote(name) + " " + strconv.Quote(arg));
+}
